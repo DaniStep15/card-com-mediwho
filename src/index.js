4b9735ecb774b8c4f34abef958d693543ceeff60
Danil Stepanov <daniladan689@gmail.com>
2024-10-10 20:02:26 +0300

    webpack config react

--- src/index.js
+++
@@ -1,6 +0,0 @@
-console.log('hello world');
-
-console.log('hello served');
-
-console.log('hello served');
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = (env, argv) =>{
     return {
         mode: env.mode || "development",
-        entry: path.resolve(__dirname, './src/index.js'),
+        entry: path.resolve(__dirname, './src/index.jsx'),
         output: {
             path: path.resolve(__dirname, './build'),
             filename: '[name][contenthash].js',
@@ -15,10 +16,39 @@ module.exports = (env, argv) =>{
             new HtmlWebpackPlugin(
                 { template: path.resolve(__dirname, './public/index.html') }
             ),
-            new webpack.ProgressPlugin()
+            new webpack.ProgressPlugin(),
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css',
+            })
         ],
         module: {
-            rules: []
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        MiniCssExtractPlugin.loader,
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                    ],
+                },
+                {
+                    test: /\.m?jsx$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                '@babel/preset-react'
+                            ]
+                        }
+                    }
+                },
+            ]
         },
         resolve: {
             extensions: ['', '.js', '.jsx']
